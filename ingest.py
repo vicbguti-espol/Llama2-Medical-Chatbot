73ea8ed6ffa9735fe1b19a07fa8b3650ddad33cb
vicbguti-espol <vicbguti@espol.edu.ec>
2023-09-18 21:15:26 -0500

    I do not know what I did, but it works!!!!
    Spanish support :)

--- ingest.py
+++ ingest.py
@@ -2,6 +2,7 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter 
+from deep_translator import GoogleTranslator
 
 DATA_PATH = 'data/'
 DB_FAISS_PATH = 'vectorstore/db_faiss'
@@ -10,18 +11,29 @@ DB_FAISS_PATH = 'vectorstore/db_faiss'
 def create_vector_db():
     loader = DirectoryLoader(DATA_PATH,
                              glob='*.pdf',
-                             loader_cls=PyPDFLoader)
+                             loader_cls=PyPDFLoader,)
 
     documents = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500,
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=400,
                                                    chunk_overlap=50)
-    texts = text_splitter.split_documents(documents)
+    # print('Getting docs...')
+    docs = text_splitter.split_documents(documents)
+    print(docs[0])
+    # texts = [x.page_content for x in docs]
+    # print('Getting translations...')
+    # ts_texts = [GoogleTranslator(source='auto', target='en').translate(text=x) for x in texts]
 
-    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
-                                       model_kwargs={'device': 'cpu'})
+    # embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
+    #                                    model_kwargs={'device': 'cpu'})
+    # embeddings = HuggingFaceEmbeddings(model_name='intfloat/multilingual-e5-large',
+    #                                    model_kwargs={'device': 'cpu'})
+    # embeddings = HuggingFaceEmbeddings(model_name='intfloat/multilingual-e5-small',
+    #                                    model_kwargs={'device': 'cpu'})
 
-    db = FAISS.from_documents(texts, embeddings)
-    db.save_local(DB_FAISS_PATH)
+
+    # db = FAISS.from_documents(docs, embeddings)
+    # # db = FAISS.from_texts(ts_texts, embeddings)
+    # db.save_local(DB_FAISS_PATH)
 
 if __name__ == "__main__":
     create_vector_db()
--- model.py
+++ model.py
@@ -5,17 +5,30 @@ from langchain.vectorstores import FAISS
 from langchain.llms import CTransformers
 from langchain.chains import RetrievalQA
 import chainlit as cl
+from deep_translator import GoogleTranslator
+import time
+
 
 DB_FAISS_PATH = 'vectorstore/db_faiss'
 
-custom_prompt_template = """Use the following pieces of information to answer the user's question.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
+# custom_prompt_template = """Use the following pieces of information to answer the user's question.
+# If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
-Context: {context}
-Question: {question}
+# Context: {context}
+# Question: {question}
 
-Only return the helpful answer below and nothing else.
-Helpful answer:
+# Only return the helpful answer below and nothing else.
+# Helpful answer:
+# """
+
+custom_prompt_template = """Utilice la siguiente información para responder la pregunta del usuario.
+Si no sabe la respuesta, simplemente diga que no la sabe, no intente inventar una respuesta.
+
+Contexto: {context}
+Pregunta: {question}
+
+Solo devuelva la útil respuesta a continuación y nada más.
+Respuesta útil:
 """
 
 def set_custom_prompt():
@@ -49,11 +62,16 @@ def load_llm():
 
 #QA Model Function
 def qa_bot():
-    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
+    # embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
+    #                                    model_kwargs={'device': 'cpu'})
+    # embeddings = HuggingFaceEmbeddings(model_name="intfloat/multilingual-e5-large",
+    #                                    model_kwargs={'device': 'cpu'})
+    embeddings = HuggingFaceEmbeddings(model_name="intfloat/multilingual-e5-small",
                                        model_kwargs={'device': 'cpu'})
     db = FAISS.load_local(DB_FAISS_PATH, embeddings)
     llm = load_llm()
     qa_prompt = set_custom_prompt()
+    
     qa = retrieval_qa_chain(llm, qa_prompt, db)
 
     return qa
@@ -64,6 +82,11 @@ def final_result(query):
     response = qa_result({'query': query})
     return response
 
+
+# start_time = time.time()
+# result = final_result('¿Para quién es la salud mental?')
+# print(result)
+# print("--- %s seconds ---" % (time.time() - start_time))
 #chainlit code
 @cl.on_chat_start
 async def start():
@@ -82,8 +105,10 @@ async def main(message):
         stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
     )
     cb.answer_reached = True
+    # message = GoogleTranslator(source='auto', target='en').translate(text=raw_message) # added line
     res = await chain.acall(message, callbacks=[cb])
     answer = res["result"]
+    # answer = GoogleTranslator(source='auto', target='es').translate(text=res["result"]) 
     sources = res["source_documents"]
 
     if sources:
